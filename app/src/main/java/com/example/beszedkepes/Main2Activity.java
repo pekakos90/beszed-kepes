8e9959e505e068a43e3acd05b4590a425f985acc
pekakos90 <pekakos90@gmail.com>
2019-03-27 23:46:59 +0100

    Beszed-Kepes

--- app/src/main/java/com/example/beszedkepes/Main2Activity.java
+++ app/src/main/java/com/example/beszedkepes/Main2Activity.java
@@ -4,9 +4,6 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
@@ -20,10 +17,9 @@ import java.util.ArrayList;
 public class Main2Activity extends AppCompatActivity {
 
     private ImageView Family_oldal, button_kamera, Image_View1,
-            Image01, Image_View2, Image_View3, Image_View4,
-            Image02, Image03, Image04, Image05, Image06, Image07, Image08, Image09,
-            Image010, Image011, Image012;
-
+            Image_10, Image_View2, Image_View3, Image_View4,
+            Image_11, Image_12, Image_13, Image_20, Image_21, Image_22, Image_23, Image_30,
+            Image_31, Image_32, Image_33;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +27,6 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         init();
-
 
 
         Family_oldal.setOnClickListener(new View.OnClickListener() {
@@ -53,13 +48,21 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
-        Image01.setOnClickListener(new View.OnClickListener() {
+        Image_10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Image_View1.setImageResource(R.mipmap.girl);
-                if(Image_View1 == Image01) {
-                    Image01.setImageResource(R.mipmap.backpain);
-                }
+                Image_10.setImageResource(R.mipmap.backpain);
+                Image_10.setEnabled(false);
+            }
+        });
+
+        Image_View1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Image_10.setImageResource(R.mipmap.girl);
+                Image_View1.setImageResource(0);
+                Image_10.setEnabled(true);
             }
         });
 
@@ -73,17 +76,19 @@ public class Main2Activity extends AppCompatActivity {
             Image_View2 = (ImageView) findViewById(R.id.Image_View2);
             Image_View3 = (ImageView) findViewById(R.id.Image_View3);
             Image_View4 = (ImageView) findViewById(R.id.Image_View4);
-            Image01 = (ImageView) findViewById(R.id.Image01);
-            Image02 = (ImageView) findViewById(R.id.Image02);
-            Image03 = (ImageView) findViewById(R.id.Image03);
-            Image04 = (ImageView) findViewById(R.id.Image04);
-            Image05 = (ImageView) findViewById(R.id.Image05);
-            Image06 = (ImageView) findViewById(R.id.Image06);
-            Image07 = (ImageView) findViewById(R.id.Image07);
-            Image08 = (ImageView) findViewById(R.id.Image08);
-            Image09 = (ImageView) findViewById(R.id.Image09);
-            Image010 = (ImageView) findViewById(R.id.Image010);
-            Image011 = (ImageView) findViewById(R.id.Image011);
-            Image012 = (ImageView) findViewById(R.id.Image012);
+            Image_10 = (ImageView) findViewById(R.id.Image_10);
+            Image_11 = (ImageView) findViewById(R.id.Image_11);
+            Image_12 = (ImageView) findViewById(R.id.Image_12);
+            Image_13 = (ImageView) findViewById(R.id.Image_13);
+            Image_20 = (ImageView) findViewById(R.id.Image_20);
+            Image_21 = (ImageView) findViewById(R.id.Image_21);
+            Image_22 = (ImageView) findViewById(R.id.Image_22);
+            Image_23 = (ImageView) findViewById(R.id.Image_23);
+            Image_30 = (ImageView) findViewById(R.id.Image_30);
+            Image_31 = (ImageView) findViewById(R.id.Image_31);
+            Image_32 = (ImageView) findViewById(R.id.Image_32);
+            Image_33 = (ImageView) findViewById(R.id.Image_33);
         }
-    }
+
+
+}
